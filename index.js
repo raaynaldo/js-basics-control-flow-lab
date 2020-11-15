d9d3a0257273780d551a4e13ac38406598534b36
raaynaldo <raaynaldo@hotmail.com>
2020-11-15 14:21:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(range) {
   // Write your code here!
+  if (range <= 400) {
+    return "This one is on me!";
+  } else if (range > 2500) {
+    return "No can do.";
+  } else {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else if (city == "Pittsburgh") {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+  }
+  if (tip === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
